c4ec45f5e7778281f86db3ca74c715f4b95f0098
VenkateshSrinivasan <venkatesh-5.srinivasan-5@cognizant.com>
2020-08-18 19:19:25 +0530

    typed the code for multichoice question.need to check

---
+++ Client/InterApp/IQuizControl.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorComponent.Shared;
+namespace BlazorComponent.Client.InterApp
+{
+    public interface IQuizControl
+    {
+
+        string NextQuestionOrDone { set; get; }
+        EventCallback<bool> OnCompleted { get; set; }
+        QAndA QuestionSet { set; get; }
+        public async Task ValidationComplete()
+        {
+            await OnCompleted.InvokeAsync(true);
+        }
+
+    }
+}
